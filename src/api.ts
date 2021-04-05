ffa3295396b84ab1f297e712edcfdc0bf93c06bd
umetsu <umetsu.kentaro@adways.net>
2021-04-05 19:47:31 +0900

    :recycle: firebaseを初期化する場所の変更

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,5 @@
 import firebase from 'firebase'
 
-const firebaseConfig = {
-  apiKey: `${process.env.API_KEY}`,
-  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.PROJECT_ID}-default-rtdb.firebaseio.com/`,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-}
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-}
-
 interface Task {
   name: string
   completed: boolean
