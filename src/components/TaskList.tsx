dd1fdeba98cdcf80472f7df1f0ce0efc768d12c9
umetsu <umetsu.kentaro@adways.net>
2021-04-21 07:00:50 +0900

    :art: 完了タスクの折りたたみをできるようにした

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,16 @@
-import { List } from '@material-ui/core'
-import React from 'react'
+import {
+  Collapse,
+  createStyles,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+  Theme,
+} from '@material-ui/core'
+import React, { useCallback, useState } from 'react'
 import { TaskItem } from './TaskItem'
 import { Task } from '../model'
+import { ExpandLess, ExpandMore } from '@material-ui/icons'
 
 interface TaskListProps {
   uncompletedTasks: ReadonlyArray<Task>
@@ -9,14 +18,46 @@ interface TaskListProps {
 }
 
 export function TaskList({ uncompletedTasks, completedTasks }: TaskListProps) {
+  const classes = useStyles()
+
+  const [open, setOpen] = useState(false)
+  const handleClick = useCallback(() => {
+    setOpen((open) => !open)
+  }, [])
+
+  const numOfCompletedTasks = completedTasks.length
+  const showCompletedTasks = numOfCompletedTasks > 0
+
   return (
     <List>
       {uncompletedTasks.map((task) => (
         <TaskItem key={task.id} task={task} />
       ))}
-      {completedTasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
+      {showCompletedTasks && (
+        <div className={classes.completed}>
+          <ListItem button onClick={handleClick}>
+            <ListItemText>
+              完了したタスク ({numOfCompletedTasks}件)
+            </ListItemText>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List disablePadding>
+              {completedTasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+              ))}
+            </List>
+          </Collapse>
+        </div>
+      )}
     </List>
   )
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    completed: {
+      marginTop: theme.spacing(2),
+    },
+  })
+)
