6c851b8a34ab100d41c48334d053d1519400f5d9
umetsu <umetsu.kentaro@adways.net>
2021-04-24 08:34:33 +0900

    :recycle: 未ログイン時の画面遷移処理をhooksへ移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { useAuth } from '../hooks/useAuth'
+import { useRequireAuth } from '../hooks/useRequireAuth'
 import { AuthorizedContent } from '../components/AuthorizedContent'
 import { FullPageSpinner } from '../components/FullPageSpinner'
-import { useRouter } from 'next/router'
 
 export default function TopPage() {
-  const { loading, loggedIn, logout } = useAuth()
-  const router = useRouter()
-
-  if (!loading && !loggedIn) {
-    void router.push('/login')
-    return <></>
-  }
+  const { loading, logout } = useRequireAuth()
 
   if (loading) {
     return <FullPageSpinner />
