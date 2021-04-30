b698d9c7f6b342cdd846ccf120540445a587edf7
umetsu <umetsu.kentaro@adways.net>
2021-04-30 14:51:08 +0900

    :recycle: Contentコンポーネント削除

--- src/pages/edit/[taskId].tsx
+++ src/pages/edit/[taskId].tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { TaskEditContent } from '../../features/tasks/TaskEditContent'
 import { ErrorBoundary } from 'react-error-boundary'
+import {
+  AppBar,
+  Box,
+  Container,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+} from '@material-ui/core'
+import Link from 'next/link'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { FullPageSpinner } from '../../common/FullPageSpinner'
+import { TaskEditForm } from '../../features/tasks/TaskEditForm'
 
 export default function EditPage() {
+  const classes = useStyles()
   const router = useRouter()
   const { taskId } = router.query
 
@@ -17,7 +31,31 @@ export default function EditPage() {
       fallback={<div>エラーが発生しました</div>}
       onError={(error, info) => console.error(error, info)}
     >
-      <TaskEditContent taskId={taskId as string} />
+      <Box display={'flex'} flexDirection={'column'}>
+        <AppBar position="static" color={'transparent'} elevation={0}>
+          <Toolbar>
+            <Link href={'/'}>
+              <IconButton edge="start" color="inherit">
+                <ArrowBackIcon />
+              </IconButton>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth={'md'} className={classes.container}>
+          <React.Suspense fallback={<FullPageSpinner />}>
+            <TaskEditForm taskId={taskId as string} />
+          </React.Suspense>
+        </Container>
+      </Box>
     </ErrorBoundary>
   )
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      height: '100%',
+      padding: theme.spacing(2),
+    },
+  })
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { TaskListContent } from '../features/tasks/TaskListContent'
 import { useDeleteAllCompletedTasks } from '../features/tasks/hooks'
 import { useUser } from '../features/auth/hooks'
 import { ErrorBoundary } from 'react-error-boundary'
+import {
+  AppBar,
+  Box,
+  Container,
+  createStyles,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import { FullPageSpinner } from '../common/FullPageSpinner'
+import { TaskList } from '../features/tasks/TaskList'
+import { TaskCreateForm } from '../features/tasks/TaskCreateForm'
+import { BottomAppBar } from '../features/tasks/BottomAppBar'
 
 export default function TopPage() {
+  const classes = useStyles()
   const { logout } = useUser()
   const { deleteAllCompletedTasks } = useDeleteAllCompletedTasks()
 
@@ -13,10 +26,39 @@ export default function TopPage() {
       fallback={<div>エラーが発生しました</div>}
       onError={console.error}
     >
-      <TaskListContent
-        deleteAllCompletedTasks={deleteAllCompletedTasks}
-        logout={logout}
-      />
+      <Box display={'flex'} flexDirection={'column'} className={classes.box}>
+        <AppBar position="static" color={'transparent'} elevation={0}>
+          <Toolbar>
+            <Typography variant={'h6'} noWrap>
+              Todo
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth={'md'} className={classes.container}>
+          <React.Suspense fallback={<FullPageSpinner />}>
+            <TaskList />
+          </React.Suspense>
+          <TaskCreateForm />
+        </Container>
+        <BottomAppBar
+          onDeleteAllCompletedTasksClick={deleteAllCompletedTasks}
+          onLogoutClick={logout}
+        />
+      </Box>
     </ErrorBoundary>
   )
 }
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    box: {
+      height: '100%',
+    },
+    container: {
+      height: '100%',
+      flex: 1,
+      overflow: 'scroll',
+      padding: 0,
+    },
+  })
+)
